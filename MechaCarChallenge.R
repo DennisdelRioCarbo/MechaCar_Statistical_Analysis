8e1e69202e3aea6a88157ba6cfcfa6b39af9b9b0
DennisdelRioCarbo <104289098+DennisdelRioCarbo@users.noreply.github.com>
2022-08-13 11:53:58 -0700

    updates file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,23 +1,26 @@
-
-```{r}
+#Deliverable 1. Linear regression to predict MPG
 library(dplyr)
-```
 
-
-```{r}
 mechacar <- read.csv(file='MechaCar_mpg.csv', stringsAsFactors = F)
-```
 
-```{r}
 head(mechacar)
-```
 
-```{r}
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar)
-```
-```{r}
+
+
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar))
-```
 
 
+#Deliverable 2. Summary Statistics on Suspension Coils
+suspension_coil <- read.csv(file='Suspension_Coil.csv',stringsAsFactors = F)
+
+view(suspension_coil)
+
+total_summary <- suspension_coil %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+view(total_summary)
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+view(lot_summary)
 
